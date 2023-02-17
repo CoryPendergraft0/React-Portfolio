525719fa0c95ec309577473ff70a709712ca3a28
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-02-17 13:21:58 -0500

    added homepage and footer

---
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import ProjectList from "../ProjectList";
+
+export default function Portfolio () {
+    
+}
---
+++ react-portfolio/src/components/pages/Resume.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center p-2 m-2"> My Resume </h1>
+      </div>
+      <div className="min-vh-100">
+        <div className="row">
+          <h5 className="d-flex justify-content-center">
+            {" "}
+            Here is where my resume would go{" "}
+          </h5>
+        </div>
+        <div className="row">
+          <div className="d-flex justify-content-center">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              style={styles.shadow}
+            >
+              <img src={resumeImg} alt="resume" style={styles.img} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
