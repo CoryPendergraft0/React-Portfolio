d9335b7aaf8ac64514dc4a7f8656d274e8179755
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-02-21 20:30:41 -0500

    removed node modules

--- react-portfolio/src/components/pages/index.js
+++
@@ -1,8 +0,0 @@
-import About from "./About";
-import Portfolio from "./Portfolio";
-import Resume from "./Resume";
-import Contact from "./Contact";
-
-const pages = { About, Portfolio, Resume, Contact };
-
-export default pages;
--- react-portfolio/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "bootswatch/dist/sketchy/bootstrap.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
